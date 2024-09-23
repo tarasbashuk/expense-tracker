ccc7be9bf6cc5d3626d13a740a70c1e9a73ac509
Taras Bashuk <bashuk.taras@gmail.com>
2024-09-23 14:07:37 +0200

    Extend apple icons set

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,15 +46,25 @@ export const metadata: Metadata = {
       {
         rel: 'icon',
         url: '/favicon.ico',
-        sizes: 'any', // for any size
+        sizes: 'any',
       },
       {
         rel: 'mask-icon',
         url: '/mask-icon.svg',
-        color: '#5bbad5', // The color you want for the icon
+        color: '#5bbad5',
       },
     ],
-    apple: '/apple-touch-icon.png', // Ensure this is 180x180
+    apple: [
+      { url: '/apple-icon-57x57.png', sizes: '57x57' },
+      { url: '/apple-icon-60x60.png', sizes: '60x60' },
+      { url: '/apple-icon-72x72.png', sizes: '72x72' },
+      { url: '/apple-icon-76x76.png', sizes: '76x76' },
+      { url: '/apple-icon-114x114.png', sizes: '114x114' },
+      { url: '/apple-icon-120x120.png', sizes: '120x120' },
+      { url: '/apple-icon-144x144.png', sizes: '144x144' },
+      { url: '/apple-icon-152x152.png', sizes: '152x152' },
+      { url: '/apple-icon-180x180.png', sizes: '180x180' },
+    ],
     other: {
       rel: 'manifest',
       url: '/manifest.webmanifest',
