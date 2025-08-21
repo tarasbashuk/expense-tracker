b804cfd98838811a0135f8ae9fe355af1ddddddf
Taras Bashuk <taras.bashuk@finalsite.com>
2025-08-21 18:47:51 +0200

    feat: add WelcomeMessage component for personalized greetings
    - Introduced a new WelcomeMessage component that utilizes react-intl for dynamic localization of welcome messages.
    - The component accepts a firstName prop and displays a personalized greeting based on the user's name.
    - Enhanced user experience with localized welcome messages in the application.

---
+++ src/components/WelcomeMessage.tsx
@@ -0,0 +1,22 @@
+'use client';
+import { Typography } from '@mui/material';
+import { useIntl } from 'react-intl';
+
+interface Props {
+  firstName: string | null;
+}
+
+const WelcomeMessage = ({ firstName }: Props) => {
+  const { formatMessage } = useIntl();
+
+  return (
+    <Typography variant="h4" component="h3" gutterBottom>
+      {formatMessage(
+        { id: 'home.welcome', defaultMessage: 'Welcome, {name}' },
+        { name: firstName || '' }
+      )}
+    </Typography>
+  );
+};
+
+export default WelcomeMessage;
