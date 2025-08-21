89c1eafc052667fb9dde8e80c6b7098ebbb26213
Taras Bashuk <taras.bashuk@finalsite.com>
2025-08-21 16:07:01 +0200

    feat: implement internationalization for transaction-related components
    - Updated AdditionalBalanceInfo, TransactionList, TransactionsDataGrid, YearlyStatsContent, AddTransactionModal, and AddTransactionModalView components to utilize react-intl for dynamic text rendering.
    - Enhanced user experience with localized labels and messages across various components.
    - Added new localization keys in en-US and uk-UA JSON files to support the updated text.

---
+++ src/components/BalanceView.tsx
@@ -0,0 +1,37 @@
+'use client';
+import { Typography, Stack } from '@mui/material';
+import HistoryIcon from '@mui/icons-material/History';
+import { FormattedMessage } from 'react-intl';
+import { formatCurrency } from '@/lib/formatCurrency';
+import { Currency } from '@prisma/client';
+
+interface Props {
+  balance?: string;
+  initialAmount?: string;
+  defaultCurrency?: Currency;
+}
+
+const BalanceClient = ({ balance, initialAmount, defaultCurrency }: Props) => {
+  return (
+    <>
+      <Typography variant="h5" component="p">
+        <FormattedMessage id="balance.title" defaultMessage="Your balance" />
+      </Typography>
+      <Stack direction="row" alignItems="center" spacing={1}>
+        <HistoryIcon color="info" fontSize="small" />
+        <Typography variant="subtitle2" color="text.secondary">
+          <FormattedMessage
+            id="balance.initial"
+            defaultMessage="Initial balance:"
+          />{' '}
+          {formatCurrency(Number(initialAmount), defaultCurrency)}
+        </Typography>
+      </Stack>
+      <Typography variant="h4" component="p" gutterBottom>
+        {formatCurrency(Number(balance), defaultCurrency)}
+      </Typography>
+    </>
+  );
+};
+
+export default BalanceClient;
---
+++ src/components/IncomeExpenseView.tsx
@@ -0,0 +1,148 @@
+'use client';
+import Link from 'next/link';
+import {
+  Box,
+  Card,
+  Chip,
+  CardContent,
+  Divider,
+  Typography,
+} from '@mui/material';
+import { FormattedMessage } from 'react-intl';
+import { NavigationPath } from '@/constants/types';
+import { formatCurrency } from '@/lib/formatCurrency';
+
+interface Props {
+  income?: number | null;
+  expense?: number | null;
+  creditReceived?: number | null;
+  creditReturned?: number | null;
+}
+
+const IncomeExpenseClient = ({
+  income,
+  expense,
+  creditReceived,
+  creditReturned,
+}: Props) => {
+  return (
+    <Link href={NavigationPath.Transactions}>
+      <Card>
+        <CardContent>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Box display="flex" flexDirection="column" flex={1} p={1}>
+              <Typography variant="h6" component="div">
+                <FormattedMessage
+                  id="incomeExpense.income"
+                  defaultMessage="Income"
+                />
+              </Typography>
+              <Typography
+                variant="h5"
+                component="div"
+                sx={{ color: 'success.main' }}
+              >
+                {formatCurrency(income ?? 0)}
+              </Typography>
+            </Box>
+
+            <Divider orientation="vertical" flexItem sx={{ marginX: '1px' }} />
+
+            <Box display="flex" flexDirection="column" flex={1} p={1}>
+              <Typography variant="h6" component="div">
+                <FormattedMessage
+                  id="incomeExpense.expense"
+                  defaultMessage="Expense"
+                />
+              </Typography>
+              <Typography
+                variant="h5"
+                component="div"
+                sx={{ color: 'error.main' }}
+              >
+                {formatCurrency(expense ?? 0)}
+              </Typography>
+            </Box>
+          </Box>
+
+          <Box
+            width={'100%'}
+            display="flex"
+            justifyContent="flex-start"
+            flex={1}
+          >
+            <Chip
+              label={
+                <FormattedMessage id="common.plus" defaultMessage="plus:" />
+              }
+              color="error"
+              variant="outlined"
+              size="small"
+              sx={{ flex: '1 1 50%', mr: 1 }}
+            />
+            <Chip
+              label={
+                <FormattedMessage id="common.plus" defaultMessage="plus:" />
+              }
+              color="success"
+              variant="outlined"
+              size="small"
+              sx={{ flex: '1 1 50%' }}
+            />
+          </Box>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Box display="flex" flexDirection="column" flex={1} p={1}>
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{ whiteSpace: 'nowrap' }}
+              >
+                <FormattedMessage
+                  id="incomeExpense.creditReceived"
+                  defaultMessage="credit received"
+                />
+              </Typography>
+              <Typography
+                variant="h5"
+                component="div"
+                sx={{ color: 'error.main' }}
+              >
+                {formatCurrency(creditReceived ?? 0)}
+              </Typography>
+            </Box>
+            <Divider orientation="vertical" flexItem sx={{ marginX: '1px' }} />
+            <Box display="flex" flexDirection="column" flex={1} p={1}>
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{ whiteSpace: 'nowrap' }}
+              >
+                <FormattedMessage
+                  id="incomeExpense.creditReturned"
+                  defaultMessage="credit returned"
+                />
+              </Typography>
+              <Typography
+                variant="h5"
+                component="div"
+                sx={{ color: 'success.main' }}
+              >
+                {formatCurrency(creditReturned ?? 0)}
+              </Typography>
+            </Box>
+          </Box>
+        </CardContent>
+      </Card>
+    </Link>
+  );
+};
+
+export default IncomeExpenseClient;
