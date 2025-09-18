be0f70fdcc08424d2f4e718aa78442dfa0cd77c7
Taras Bashuk <taras.bashuk@finalsite.com>
2025-09-18 10:34:39 +0200

    Revert "Test commit. Will be reverted"
    This reverts commit 7190f299a1f662e534c37028aa5ef1d86cd14b94.

--- src/components/WelcomeMessage.tsx
+++ src/components/WelcomeMessage.tsx
@@ -8,26 +8,14 @@ interface Props {
 
 const WelcomeMessage = ({ firstName }: Props) => {
   const { formatMessage } = useIntl();
-  const smallScreen =
-    window.matchMedia('(max-width: 480px)').matches &&
-    window.devicePixelRatio === 1;
-
-  const matchMedia = window.matchMedia('(max-width: 480px)').matches;
 
   return (
-    <>
-      Small screen - {`${smallScreen}`}
-      <br />
-      matchMedia 480 - {`${matchMedia}`}
-      <br />
-      DPR - {window.devicePixelRatio}
-      <Typography variant="h4" component="h3" gutterBottom>
-        {formatMessage(
-          { id: 'home.welcome', defaultMessage: 'Welcome, {name}' },
-          { name: firstName || '' },
-        )}
-      </Typography>
-    </>
+    <Typography variant="h4" component="h3" gutterBottom>
+      {formatMessage(
+        { id: 'home.welcome', defaultMessage: 'Welcome, {name}' },
+        { name: firstName || '' },
+      )}
+    </Typography>
   );
 };
 
