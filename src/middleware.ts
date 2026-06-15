4857b447aae05c500bef1ec90f294e543bec4639
Taras Bashuknpm <bashuk.taras@gmail.com>
2026-06-15 10:22:55 +0200

    feat: Update middleware matcher for improved route handling

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,5 +9,8 @@ export default clerkMiddleware({
 });
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
 };
